c6f6c941cb03ae5e1f4048aa6a91efb88e375efc
sprhawk <hongbo@yang.me>
2011-11-30 14:15:28 +0800

    added the first snippet

---
+++ properties.h
@@ -0,0 +1,9 @@
+@interface dummy : NSObject
+
+//I set shortcut 'assign' to it
+@property (nonatomic, assign, <#read/write#>) <#Class#> <#property#>;
+
+//I set short 'copy' to it
+@property (nonatomic, copy, <#read/write#>) <#Class#> <#property#>;
+
+@end
